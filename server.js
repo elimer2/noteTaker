f0a59be583ba4f5b9bdad2c4d3fe6bbc25494203
lindyem <lindy.merwin@gmail.com>
2020-07-11 11:34:11 -0700

    api

--- server.js
+++ server.js
@@ -8,19 +8,21 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-let notes = [];
-
 function readDB() {
-  fs.readFile(__dirname + '/db/db.json', (err, data) => {
-    notes = data;
-  });
+  const notes = fs.readFileSync(__dirname + "/db/db.json");
+  const parsedNotes = JSON.parse(notes);
+  return parsedNotes;
 }
 
-function writeDB() {
-  fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notes, null, '\t'), () => {});
+function writeDB(newNote) {
+  const notes = readDB();
+  notes.push(newNote);
+  fs.writeFile(
+    __dirname + "/db/db.json",
+    JSON.stringify(notes, null, "\t"),
+    () => {}
+  );
 }
-
-readDB();
 
 // Routes
 app.get("/", (req, res) => {
@@ -31,14 +33,19 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-// app.get("/api/notes", (req, res) => {
-//   return res.json(db.json); // ??????
-// });
-// app.post("/api/notes", function (req, res) {
-//   res.send("POST request to the homepage");
-//   console.log(req.body);
-//   res.json(newCharacter); //????
-// });
+app.get("/api/notes", (req, res) => {
+  const notes = readDB();
+  return res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  writeDB(newNote);
+  res.json({
+    status: "Success",
+    message: "Note Saved",
+  });
+});
 
 //Listener
 app.listen(PORT, function () {
